3902ed3210381fab0179648497d94f951a38db50
Daniel Augusto <daniel.aug.souza@gmail.com>
2025-10-29 01:04:28 -0300

    Add app to fullscreen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import MeuEstoque from "./MeuEstoque";
 import MinhasVendas from "./MinhasVendas";
+import { useEffect } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
 
 const Drawer = createDrawerNavigator();
 
 export default function Index() {
 
+	useEffect(() => {
+		NavigationBar.setVisibilityAsync('hidden');
+	});
     return (
         <Drawer.Navigator>
             <Drawer.Screen name="Meu Estoque" component={MeuEstoque} />
